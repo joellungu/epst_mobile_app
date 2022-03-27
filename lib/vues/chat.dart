ef184147998a867d583a555e2b247a642fb4c237
Joel Lungu <lungujoel138@gmail.com>
2022-03-27 16:01:14 +0100

    ajout du tchat 3/27/2022-16h01

--- lib/vues/chat.dart
+++ lib/vues/chat.dart
@@ -9,6 +9,8 @@ import 'package:flutter_chat_bubble/chat_bubble.dart';
 import 'package:flutter_chat_bubble/clippers/chat_bubble_clipper_1.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
+var _channel;
+
 class Chat extends StatefulWidget {
   String? titre;
   String? nom;
@@ -21,182 +23,236 @@ class Chat extends StatefulWidget {
 }
 
 class _Chat extends State<Chat> {
-  var _channel;
-
   List<Widget> listeConv = [];
 
-  TextEditingController chatCont = TextEditingController();
+  Widget? chatt;
 
   @override
   void initState() {
     //
     _channel = WebSocketChannel.connect(
-      Uri.parse('ws://localhost:8080/chat/${widget.nom}'),
+      Uri.parse('ws://10.0.2.2:8080/chat/tatu'), //${widget.nom}192.168.43.2
     );
+    //
+    _channel.stream.listen((message) {
+      //
+      Map<String, dynamic> map = jsonDecode((message) as String);
+      print("la reponse du serveur: $map");
+      String idsession = map["idsession"] ?? map["requete"];
+
+      //
+      String idSessionHote = map["idSessionHote"] ?? "";
+      //
+      String contenu = map["contenu"] ?? "";
+      contenu != "" ? listeConv.add(smsMessage(false, contenu)) : "";
+      setState(() {
+        if (idsession == "start") {
+          chatt = ChattConv(idSessionHote, listeConv);
+        }
+      });
+    });
+
+    print(_channel);
+    chatt = Center(
+      child: Container(
+        height: 100,
+        //width: 200,
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              height: 5,
+              width: 100,
+              alignment: Alignment.center,
+              child: LinearProgressIndicator(),
+            ),
+            Text("Etablissement de la communication avec un agent de la DGC")
+          ],
+        ),
+      ),
+    );
+
     //
     super.initState();
   }
 
   @override
+  void dispose() {
+    //_channel.closeReason = "";
+    _channel.sink.close(0);
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              height: 40,
-              width: 40,
-              alignment: Alignment.center,
-              child: Icon(
-                CupertinoIcons.person,
-                color: Colors.white,
-              ),
-              decoration: BoxDecoration(
-                color: Colors.white24,
-                borderRadius: BorderRadius.circular(20),
-              ),
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            Text("Agent DGC/EPST"),
-          ],
-        ),
-      ),
-      body: StreamBuilder(
-        stream: _channel.stream,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            print("t'est quoi?: ${snapshot.data}");
-            if (snapshot.hasData == null) {
-              return Text("${snapshot.data}");
-            } else {
-              //Je pourais repondre parce que j'aurai toujours sont id donc (to)
-              Map<String, dynamic> chatt =
-                  jsonDecode((snapshot.data) as String);
-              String commande = chatt["requete"];
-              String to_ = chatt["idSessionHote"];
-              String contenu = chatt["contenu"];
-
-              print(chatt);
-              //
-              if (commande == "start") {
-                listeConv.add(smsMessage(true, contenu));
-                return Column(
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: ListView(
-                        children: List.generate(
-                          listeConv.length,
-                          (index) {
-                            return listeConv[index];
-                          },
-                        ),
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 20,
-                        vertical: 10,
-                      ),
-                      decoration: BoxDecoration(
-                        color: Colors.transparent,
-                      ),
-                      child: SafeArea(
-                        child: Row(
-                          children: [
-                            Icon(
-                              Icons.mic,
-                              color: Colors.green.shade300,
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Expanded(
-                              child: Container(
-                                //height: 50,
-                                decoration: BoxDecoration(
-                                  color: Colors.grey.shade200,
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                                child: Row(
-                                  children: [
-                                    IconButton(
-                                      onPressed: () {},
-                                      icon: Icon(
-                                        Icons.attach_file,
-                                        color: Colors.blue.shade400,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      width: 10,
-                                    ),
-                                    Expanded(
-                                      flex: 1,
-                                      child: TextField(
-                                        controller: chatCont,
-                                        decoration: InputDecoration(
-                                          border: InputBorder.none,
-                                        ),
-                                      ),
-                                    ),
-                                    IconButton(
-                                      onPressed: () async {
-                                        //
-                                        await _channel.sink.add(
-                                            '{"from":"...","to":"$to_","content":"${chatCont.text}"');
-                                      },
-                                      icon: Icon(
-                                        Icons.send,
-                                        color: Colors.blue.shade400,
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    )
-                  ],
-                );
-              } else {
-                return const Center(
-                  child: Text("Mauvaise commande"),
-                );
-              }
-              //return Listage(jsonDecode((snapshot.data) as String));
-            }
-
-            //
-          } else {
-            return Center(
-              child: Container(
-                height: 100,
-                //width: 200,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                height: 40,
+                width: 40,
                 alignment: Alignment.center,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Container(
-                      height: 5,
-                      width: 100,
-                      alignment: Alignment.center,
-                      child: LinearProgressIndicator(),
-                    ),
-                    Text(
-                        "Etablissement de la communication avec un agent de la DGC")
-                  ],
+                child: Icon(
+                  CupertinoIcons.person,
+                  color: Colors.white,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white24,
+                  borderRadius: BorderRadius.circular(20),
                 ),
               ),
-            );
-          }
-        },
-      ),
+              SizedBox(
+                width: 10,
+              ),
+              Text("Agent DGC/EPST"),
+            ],
+          ),
+        ),
+        body: chatt!);
+  }
+
+  Widget smsMessage(bool t, String contenu) {
+    if (t) {
+      return ChatBubble(
+        clipper: ChatBubbleClipper1(type: BubbleType.sendBubble),
+        alignment: Alignment.topRight,
+        margin: EdgeInsets.only(top: 20),
+        backGroundColor: Colors.blue,
+        child: Container(
+          constraints: BoxConstraints(
+            maxWidth: MediaQuery.of(context).size.width * 0.7,
+          ),
+          child: Text(
+            contenu,
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      );
+    } else {
+      return ChatBubble(
+        clipper: ChatBubbleClipper1(type: BubbleType.receiverBubble),
+        backGroundColor: Color(0xffE7E7ED),
+        margin: EdgeInsets.only(top: 20),
+        child: Container(
+          constraints: BoxConstraints(
+            maxWidth: MediaQuery.of(context).size.width * 0.7,
+          ),
+          child: Text(
+            contenu,
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      );
+    }
+  }
+}
+
+class ChattConv extends StatefulWidget {
+  List? listeConv;
+  String? idSessionHote;
+
+  ChattConv(this.idSessionHote, this.listeConv);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ChattConv();
+  }
+}
+
+class _ChattConv extends State<ChattConv> {
+  TextEditingController chatCont = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          flex: 1,
+          child: ListView(
+            children: List.generate(
+              widget.listeConv!.length,
+              (index) {
+                return widget.listeConv![index];
+              },
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 10,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.transparent,
+          ),
+          child: SafeArea(
+            child: Row(
+              children: [
+                Icon(
+                  Icons.mic,
+                  color: Colors.green.shade300,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Container(
+                    //height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade200,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.attach_file,
+                            color: Colors.blue.shade400,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: TextField(
+                            controller: chatCont,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () async {
+                            //
+                            setState(() {
+                              widget.listeConv!
+                                  .add(smsMessage(true, chatCont.text));
+                              _channel.sink.add(
+                                  """{"from":"...","to":"${widget.idSessionHote}","content":"${chatCont.text}" }""");
+                              chatCont.clear();
+                            });
+                          },
+                          icon: Icon(
+                            Icons.send,
+                            color: Colors.blue.shade400,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 
